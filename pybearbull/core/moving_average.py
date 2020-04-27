15d4f617350ca6c803868a72b04d92c375c0efc8
Sam Hentschel <samh95@vt.edu>
2020-04-26 22:40:55 -0400

    updated github workflow; updated .gitignore; added simple moving average code and testing

--- pybearbull/core/moving_average.py
+++ pybearbull/core/moving_average.py
@@ -1,7 +1,27 @@
+# This file is part of the ManBearBull/PyBearBull project.
+# Copyright (C) 2020 Sam Hentschel (bl1nk1n)
+# 
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT 
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+# 
+# You should have received a copy of the GNU Affero General Public License along 
+# with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
 #
 # Takes in an array of numbers and returns a simple average of them.
 # Parameters:
 #               data    -   array of numbers
+#                       -   must not be empty
+#
 # Return:
 #           - If the number of data elements is greater than 0, returns the
 #             average of the numbers in the array.
@@ -13,8 +33,36 @@ def simple_average(data):
         return None
     return sum(data) / len(data)
 
-#def simple_moving_average(data, window):
-#    pass
+#
+# Takes in a dictionary of {time: number} pairs and returns a dictionary of
+# {date: simple moving average} pairs.
+# Parameters:
+#               data    -   dictionary of {time: number} pairs
+#                       -   must not be empty
+#
+#               window  -   number of time periods for this simple moving
+#                           average to cover
+#                       -   must be greater than 0, but less than or equal to
+#                           the size of the data
+#
+# Return:
+#           - If the number of data elements is greater than 0, and the window
+#             is greater than 0 but less than or equal to the data size, returns
+#             a dictionary of times and simple moving averages.
+#
+#           - If the number of data elements is equal to 0, the window size is
+#             greater than the amount of data, or the window size is less than
+#             1, returns None.
+#
+def simple_moving_average(data, window):
+    if (len(data) == 0 or window > len(data) or window <= 0):
+        return None
 
-#def exponential_moving_average(data, window, smoothing):
-#    pass
+    keys = list(data.keys())
+    values = list(data.values())
+    sma = {}
+
+    for i in range(0, (len(keys) - window) + 1):
+        sma[keys[i + (window - 1)]] = simple_average(values[i:i + window])
+
+    return sma
--- tests/unit/core/moving_average_test.py
+++ tests/unit/core/moving_average_test.py
@@ -1,42 +1,92 @@
-from pybearbull.core.moving_average import simple_average;
+# This file is part of the ManBearBull/PyBearBull project.
+# Copyright (C) 2020 Sam Hentschel (bl1nk1n)
+# 
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT 
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+# 
+# You should have received a copy of the GNU Affero General Public License along 
+# with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+from pybearbull.core.moving_average import simple_average, simple_moving_average
 
 class Test_simple_average:
-    def test_simple_average_no_data(self):
+    def test_simple_average_empty(self):
         data = []
         assert simple_average(data) == None
 
-    def test_simple_average_two_zero(self):
+    def test_simple_average_both_zero(self):
         data = [0, 0]
         assert simple_average(data) == 0
 
-    def test_simple_average_many_zero(self):
-        data = [ 0 for i in range(0, 100) ]
-        assert simple_average(data) == 0
-
-    def test_simple_average_two_positive(self):
+    def test_simple_average_both_positive(self):
         data = [1, 1]
         assert simple_average(data) == 1
 
-    def test_simple_average_many_small_positive(self):
-        data = [ 1 for i in range(0, 20000, 200) ]
-        assert simple_average(data) == 1
-
-    def test_simple_average_many_large_positive(self):
-        data = [ i for i in range(20000, 100000, 200) ]
-        assert simple_average(data) == 59900
-
-    def test_simple_average_two_negative(self):
+    def test_simple_average_both_negative(self):
         data = [-1, -1]
         assert simple_average(data) == -1
 
-    def test_simple_average_many_small_negative(self):
-        data = [ -1 for i in range(0, 20000, 200) ]
+    def test_simple_average_many_zero(self):
+        data = [0 for i in range(0, 100)]
+        assert simple_average(data) == 0
+
+    def test_simple_average_many_positive(self):
+        data = [1 for i in range(0, 20000, 200)]
+        assert simple_average(data) == 1
+
+    def test_simple_average_many_negative(self):
+        data = [-1 for i in range(0, 20000, 200)]
         assert simple_average(data) == -1
 
+    def test_simple_average_many_large_positive(self):
+        data = [i for i in range(20000, 100000, 200)]
+        assert simple_average(data) == 59900
+
     def test_simple_average_many_large_negative(self):
-        data = [ i for i in range(-20000, -100000, -200) ]
+        data = [i for i in range(-20000, -100000, -200)]
         assert simple_average(data) == -59900
 
-    def test_simple_average_many_positive_negative(self):
-        data = [i for i in range(-100000, 101000, 1000) ]
+    def test_simple_average_many_mixed(self):
+        data = [i for i in range(-100000, 101000, 1000)]
         assert simple_average(data) == 0
+
+
+class Test_simple_moving_average:
+    def test_simple_moving_average_empty(self):
+        data = {}
+        assert simple_moving_average(data, 3) == None
+
+    def test_simple_moving_average_neg1_window_3_data(self):
+        data = {"2 days ago": 7001, "1 day ago": 7000, "today": 6999}
+        assert simple_moving_average(data, -1) == None
+
+    def test_simple_moving_average_9_window_3_data(self):
+        data = {"2 days ago": 7001, "1 day ago": 7000, "today": 6999}
+        assert simple_moving_average(data, 9) == None
+
+    def test_simple_moving_average_9_window_9_data(self):
+        data = {"8 days ago": 6999, "7 days ago": 7000, "6 days ago": 7001,
+                "5 days ago": 7000, "4 days ago": 6999, "3 days ago": 7000,
+                "2 days ago": 7001, "1 day ago": 7000, "today": 7000}
+        assert simple_moving_average(data, 9) == {"today": 7000}
+
+    def test_simple_moving_average_3_window_9_period(self):
+        data = {"8 days ago": 6999, "7 days ago": 7000, "6 days ago": 7001,
+                "5 days ago": 7000, "4 days ago": 6999, "3 days ago": 7000,
+                "2 days ago": 7001, "1 day ago": 7000, "today": 6999}
+        assert simple_moving_average(data, 3) == {"6 days ago": 7000, 
+                                               "5 days ago": 7000.333333333333,
+                                               "4 days ago": 7000,
+                                               "3 days ago": 6999.666666666667,
+                                               "2 days ago": 7000,
+                                               "1 day ago": 7000.333333333333,
+                                               "today": 7000}
